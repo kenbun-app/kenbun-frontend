60d0a7b0c14617e8ba5fb0baf0e43e8e92e75f3b
osoken <osoken@osoekawalab.com>
2023-06-10 23:12:38 +0900

    feat(app): add account menu and logout button

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -3,18 +3,21 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import MuiLink from '@mui/material/Link';
+import AccountMenu from './AccountMenu';
+import { useAuthContext } from '@/src/contexts/AuthContext';
 
 export default function AppHeader() {
-return (
-        <MuiAppBar position="static">
-        <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <MuiLink href="/" color="inherit" underline="none">
-                kenbun
-              </MuiLink>
-            </Typography>
-            <Button color="inherit">Login</Button>
-        </Toolbar>
-        </MuiAppBar>
-    );
+  const { user } = useAuthContext()
+  return (
+    <MuiAppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <MuiLink href="/" color="inherit" underline="none">
+            kenbun
+          </MuiLink>
+        </Typography>
+        {user?<AccountMenu></AccountMenu>:<Button color="inherit">Login</Button>}
+      </Toolbar>
+    </MuiAppBar>
+  );
 }
